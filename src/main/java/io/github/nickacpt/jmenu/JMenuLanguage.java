1980e8beccfcc7aa17c555925c34ee2f7d77fad3
NickAc <32451103+NickAcPT@users.noreply.github.com>
2020-06-03 20:25:24 +0100

    Add a missing colon character

--- src/main/java/io/github/nickacpt/jmenu/JMenuLanguage.java
+++ src/main/java/io/github/nickacpt/jmenu/JMenuLanguage.java
@@ -2,7 +2,7 @@ package io.github.nickacpt.jmenu;
 
 public class JMenuLanguage {
     public String INSERT_PREFIX = "Insert";
-    public String INSERT_THE_OPTION_PREFIX = "Select the option";
+    public String INSERT_THE_OPTION_PREFIX = "Select the option:";
     public String DAY_MONTH_YEAR = "(day/month/year)";
 
     public String INCORRECT_DATE = "Incorrect date. Please try again!";
--- src/main/java/io/github/nickacpt/jmenu/PortugueseJMenuLanguage.java
+++ src/main/java/io/github/nickacpt/jmenu/PortugueseJMenuLanguage.java
@@ -3,7 +3,7 @@ package io.github.nickacpt.jmenu;
 public class PortugueseJMenuLanguage extends JMenuLanguage {
     public PortugueseJMenuLanguage() {
         INSERT_PREFIX = "Insira";
-        INSERT_THE_OPTION_PREFIX = "Insira a opção";
+        INSERT_THE_OPTION_PREFIX = "Insira a opção:";
         DAY_MONTH_YEAR = "(dia/mes/ano)";
 
         INCORRECT_DATE = "Data incorreta. Por favor tente novamente!";
